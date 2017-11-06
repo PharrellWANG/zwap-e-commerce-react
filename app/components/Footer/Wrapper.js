19497cc4800beca517906cb0fde41243d6fbbca1
PharrellWANG <15113029g@connect.polyu.hk>
2017-11-06 17:22:26 +0800

    narrowed section

--- app/components/Footer/Wrapper.js
+++ app/components/Footer/Wrapper.js
@@ -3,14 +3,19 @@ import styled from 'styled-components';
 const Wrapper = styled.footer`
   display: flex;
   justify-content: space-between;
-  // padding: 3em 3em;
+  // padding: 3em 2em;
   border-top: 1px solid #666;
   
   // max-width: calc(768px + 16px * 2);
   margin: 0 auto;
   display: flex;
   min-height: 100%;
-  // padding: 0 16px;
+  padding: 3px ;
+  text-align: center;
+  // background: 'grey,
+  // height: '50px',
+  // padding: '20px',
+  // color: 'black',
   // flex-direction: column;
 `;
 
