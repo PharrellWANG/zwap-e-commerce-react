a67c6c89ed7f04d69ed40144719ad7012f84971c
Pharrell.zx <15113029g@connect.polyu.hk>
2018-02-28 23:06:49 +0800

    updates

--- app/containers/MainPage/datepicker.js
+++ app/containers/MainPage/datepicker.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { DatePicker } from 'material-ui-pickers';
-
-export default class MuiDatePicker extends React.Component {
-  state = {
-    selectedDate: new Date(),
-  }
-
-  handleDateChange = (date) => {
-    this.setState({ selectedDate: date });
-  }
-
-  render() {
-    const { selectedDate } = this.state;
-
-    return (
-      <div>
-        <DatePicker
-          value={selectedDate}
-          onChange={this.handleDateChange}
-        />
-      </div>
-    );
-  }
-}
+// import React from 'react';
+// import { DatePicker } from 'material-ui-pickers';
+//
+// export default class MuiDatePicker extends React.Component {
+//   state = {
+//     selectedDate: new Date(),
+//   }
+//
+//   handleDateChange = (date) => {
+//     this.setState({ selectedDate: date });
+//   }
+//
+//   render() {
+//     const { selectedDate } = this.state;
+//
+//     return (
+//       <div>
+//         <DatePicker
+//           value={selectedDate}
+//           onChange={this.handleDateChange}
+//         />
+//       </div>
+//     );
+//   }
+// }
